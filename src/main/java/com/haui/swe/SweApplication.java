7e56384487bd8a484de5f8ab24d0bb409d5acccd
DangDong204 <165341409+DangDong204@users.noreply.github.com>
2025-10-27 18:05:27 +0700

    Update SweApplication.java

--- src/main/java/com/haui/swe/SweApplication.java
+++ src/main/java/com/haui/swe/SweApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SweApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SweApplication.class, args);
+
+        SpringApplication.run(SweApplication.class, args);
 	}
 
 }
