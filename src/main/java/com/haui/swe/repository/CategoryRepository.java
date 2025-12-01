ac4ae05e11246625d2ee0a986b43667c7336abd0
DangDong204 <165341409+DangDong204@users.noreply.github.com>
2025-12-01 15:58:32 +0700

    Done CRUD Category

--- src/main/java/com/haui/swe/repository/CategoryRepository.java
+++ src/main/java/com/haui/swe/repository/CategoryRepository.java
@@ -3,5 +3,9 @@ package com.haui.swe.repository;
 import com.haui.swe.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category,Integer> {
+
+    List<Category> findByStatus(Boolean status);
 }
--- src/main/java/com/haui/swe/service/CategoryService.java
+++ src/main/java/com/haui/swe/service/CategoryService.java
@@ -1,8 +1,10 @@
 package com.haui.swe.service;
 
 import com.haui.swe.model.Category;
+import com.haui.swe.model.Product;
 import com.haui.swe.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,14 +16,24 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     public List<Category> findAll(){
-        return categoryRepository.findAll();
+        return categoryRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+    }
+
+    public List<Category> findAllActive() {
+        return categoryRepository.findByStatus(true);
     }
 
     public Category findById(Integer id){
         return categoryRepository.findById(id).get();
     }
 
+    // Thêm + Sửa
+    public void saveOrUpdate(Category category){
+        categoryRepository.save(category);
+    }
 
-
+    public void delete(Integer id){
+        categoryRepository.deleteById(id);
+    }
 
 }
